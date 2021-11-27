880f1198fd8a756cba2be8247e5a7ec68ee0f58a
danikagiron <danikag@kth.se>
2021-11-27 10:51:53 +0100

    List views - more pseudocode

--- js/views/userListView.js
+++ js/views/userListView.js
@@ -1,7 +1,24 @@
-function UserListView(props){
-    return(
+function UserListView(props) {
+    return (
         <div>
             /*in an unordered list, show the list of users -- each user has a profile pic. Show preview of list or nah? */
+            <div class="box">
+                <h1>Check out other users' lists!</h1>
+                {props.users.map(x =>
+                    // display: inline-block
+                    <span key={x.id}>
+                            // profile pic and user's name in the same line?
+                        <img src={x.profilepic} height={100} width={100} />
+                        <h4>{x.name}</h4>
+                        // underneath
+                        {x.list.listtitle1}
+                        {x.list.listtitle2}
+                        {x.list.listtitle3}
+                        {"..."}
+                    </span>
+
+                )}
+            </div>
         </div>
     )
 }
