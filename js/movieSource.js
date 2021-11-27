4dee9f117d037c8682d07fdd76de52affd70df72
amir <amirali.safizadeh@gmail.com>
2021-11-27 15:51:26 +0100

    movieSource added

---
+++ js/movieSource.js
@@ -0,0 +1,35 @@
+const MovieSource = {
+    apiCall(params) {
+        return fetch(BASE_URL + params, {
+            "method": "GET",
+        }).then(response => {
+            if (response.status != 200)
+                throw new Error(response.statusText);
+            else
+                return response;
+        })
+            .then(response => response.json());
+
+    }
+
+    ,
+    // returns a promise which as data has an array of objects
+    // you can get movie id, image, title  
+    searchMovie(title) {
+        return MovieSource.apiCall("SearchMovie/" + API_KEY + title).then(data => data.results);
+    }
+    ,
+    // returns a promise which as data has object that contains all the information about the movie 
+    getMovieDetails(id) {
+        return MovieSource.apiCall("Title/" + API_KEY + id);
+    }
+
+
+
+
+
+
+
+
+
+}
