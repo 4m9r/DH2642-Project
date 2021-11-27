5c4109e49891f0d05a2f204171cf5cfd4786c929
v-a-p <vaprodan@kth.se>
2021-11-27 16:22:21 +0100

    basic skeleton

---
+++ js/views/SignUpView.js
@@ -0,0 +1,29 @@
+function SignUpView(props){
+
+    return(
+      <html>
+  
+      <head>
+  
+        <title>
+          Sign up!
+        </title>
+  
+  
+      </head>
+  
+      <body>
+  
+        <h1><input type="text" id="email" placeholder="Email address"/></h1>
+        <h1><input type="text" id="emailconfirm" placeholder="Confirm Email address"/></h1>
+        <h1><input type="password" id="password" placeholder="Password"/></h1>
+        <h1><input type="password" id="passwordconfirm" placeholder="Confirm Password"/></h1>
+        <h1><input type="text" id="username" placeholder="Create a Username"/></h1>
+  
+      </body>
+  
+      </html>
+  )
+  
+    
+  }
