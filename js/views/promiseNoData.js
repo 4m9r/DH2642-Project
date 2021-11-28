98dd7ba0245b375255d7ede32732a6ee1f35f245
amir <amirali.safizadeh@gmail.com>
2021-11-28 14:32:07 +0100

    promiseNoData added

---
+++ js/views/promiseNoData.js
@@ -0,0 +1,30 @@
+function promiseNoData(promise, data, error) {
+    //case 1
+    if (promise == null)
+        return (
+            <span>no data</span>
+        )
+    else {
+        //case 2
+        if (data == undefined && error == null)
+            return (
+                <div class="loadingParent">
+                    <img class="loading" src="http://www.csc.kth.se/~cristi/loading.gif" />
+                </div>
+            )
+        //case 3
+        else if (data == null)
+            return (
+                <span>{error}</span>
+            )
+        // case 4
+        else
+            return false;
+    }
+
+
+
+
+
+
+}
