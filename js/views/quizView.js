b7a8d7bc36cbe7b9b713d7e27eb497c394bb0320
amir <amirali.safizadeh@gmail.com>
2021-11-28 18:43:18 +0100

    it works with fixed values

--- js/views/quizView.js
+++ js/views/quizView.js
@@ -1,20 +1,47 @@
 function QuizView(params) {
+    if (params.model.numberOfQuestion === 3)
+        return <div>
+            good job you did it your score is 0 you db
+        </div>
 
     return (
-        <div>{
-            params.data.map(e => {
-                return <div>
-                    <span> {e.Question} </span><div>
-                        <button>option1</button>
-                        <button>option2</button>
-                        <button>option3</button>
-                        <button>option4</button>
-                    </div>
+
+        <div>
+            <div>
+                <span> {params.data[params.model.numberOfQuestion].Question} </span>
+                <div>
+                    <button >option1</button>
+                    <button>option2</button>
+                    <button>option3</button>
+                    <button>option4</button>
+
                 </div>
+                <div>
+                    <button disabled={params.model.numberOfQuestion == 0} onClick={() =>
+                        params.model.setQuestionNumber(params.model.numberOfQuestion - 1)}>
+                        Privous
+                    </button>
+                    <button onClick={() =>
+                        params.model.setQuestionNumber(params.model.numberOfQuestion + 1)}>
+                        {
+                            btnName(params.model.numberOfQuestion)
+                        }
+                    </button>
+                </div>
+            </div>
 
-            })
+            )
 
-        }
+
         </div >
     )
+}
+
+
+function btnName(x) {
+    console.log(x)
+    if (x === 2)
+        return "Finish"
+    else
+        return "Next"
 }
