3222143b1a999cb8640b99ac68e4a7c617340a07
amir <amirali.safizadeh@gmail.com>
2021-12-04 21:48:25 +0100

    removing console

---
+++ js/vuejs/searchPresenter.js
@@ -0,0 +1,37 @@
+const SearchPresenter = {
+    data() { return { promise: null, data: null, error: null, searchQuery: "", }; },
+    props: ["model"],
+    created() { this.promise = MovieSource.searchMovie(""); },  // lifecycle 1, execute at creation
+    watch: {
+        'promise': {
+            immediate: true,
+            handler() {
+                this.data = this.error = null;
+                if (this.promise) {
+                    const p = this.promise;
+                    this.promise.then(dt => { if (this.promise === p) this.data = dt; })
+                        .catch(er => { if (this.promise === p) this.error = er; });
+                }
+
+            }
+        }
+    },
+
+    render() {
+
+        return <div>
+            <SearchView
+                onText={txt => this.searchQuery = txt}
+                onSearch={() => this.promise = MovieSource.searchMovie(this.searchQuery)}
+
+            />
+
+            {promiseNoData(this.promise, this.data, this.error) ||
+                <SearchResultsView searchResults={this.data}
+                    movieChosen={id => this.model.setCurrentMovie(id)}
+                />}
+        </div>
+    }
+
+
+}
