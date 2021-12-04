bc7a6f6598a3b57ef478932930ffa66ca316565b
amir <amirali.safizadeh@gmail.com>
2021-12-04 22:42:02 +0100

    comenting

--- js/app.js
+++ js/app.js
@@ -1,40 +1,15 @@
-// function App(props) {
-//     return (
-
-//         <div>
-//             <div>
-//                 <SearchPresenter model={props.model} />
-
-//             </div>
-
-//             <div>
-//                 <DetailsPresenter model={props.model} />
-
-//             </div>
-
-//         </div>
-
-
-
-
-//     );
-
-
-// }
-
 function App(props) {
-    
 
-    return(
+
+    return (
 
         <div>
-            <QuizPresenter model={props.model} />
-
-            <Show hash="#list"> <ListPresenter model={props.model}/> </Show>
-            <Show hash="#search"> <SearchPresenter model={props.model}/> </Show>
-            <Show hash="#details"> <DetailsPresenter model={props.model}/> </Show>
-            <Show hash="#quiz"> <QuizPresenter model={props.model}/> </Show>
-            <Show hash="#recommend"> <RecommendPresenter model={props.model}/> </Show>
+            {/* <QuizPresenter model={props.model} />
+            <Show hash="#list"> <ListPresenter model={props.model} /> </Show> */}
+            <Show hash="#search"> <SearchPresenter model={props.model} /> </Show>
+            <Show hash="#details"> <DetailsPresenter model={props.model} /> </Show>
+            {/* <Show hash="#quiz"> <QuizPresenter model={props.model}/> </Show>
+            <Show hash="#recommend"> <RecommendPresenter model={props.model}/> </Show> */}
 
         </div>
 
@@ -45,11 +20,11 @@ function App(props) {
 
 
 
-    
+
 }
 
-function defaultRoute(){
-    if(!(["#list", "#search", "#details", "#quiz", "#recommend"].find((knownRoute)=>window.location.hash==knownRoute))) window.location.hash="#search";
+function defaultRoute() {
+    if (!(["#list", "#search", "#details", "#quiz", "#recommend"].find((knownRoute) => window.location.hash == knownRoute))) window.location.hash = "#search";
 }
 
 
