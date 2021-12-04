79135720d1c13a236c6582cb2dbdbb2255a1a30c
Hastimazadeh <hastimazadeh@gmail.com>
2021-12-04 21:55:49 +0100

    deatils presenter

---
+++ js/vuejs/detailsPresenter.js
@@ -0,0 +1,8 @@
+function DetailsPresenter(props) {
+    return (promiseNoData(props.model.currentMovie, props.model.currentMovieDetails, props.model.currentMovieError) ||
+        <DetailsView 
+        data = {currentMovieDetails}
+   
+        />
+    );
+}
