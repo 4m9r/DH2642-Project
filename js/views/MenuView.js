f0733ade438943c579669bb96254fa1239ca0f32
v-a-p <vaprodan@kth.se>
2021-12-04 21:14:11 +0100

    Update MenuView.js

--- js/views/MenuView.js
+++ js/views/MenuView.js
@@ -4,14 +4,14 @@ function MenuView(props){
       <div class="menudrop">
         <button class="menubutton">Menu</button>
         <div class="menucontent">
-          <a href="#">My lists</a>
-          <a href="#">Search for a movie</a>
-          <a href="#">Recommend me a movie</a>
-          <a href="#">Take a quiz</a>
+          <a href="#list">My list</a>
+          <a href="#search">Search for a movie</a>
+          <a href="#recommend">Recommend me a movie</a>
+          <a href="#quiz">Take a quiz</a>
         </div>
       </div>
   )
   
     
-  }
+}
   
