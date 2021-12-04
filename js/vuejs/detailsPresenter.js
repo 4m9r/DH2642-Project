c9bba22055826a4c22d1b4f3fa70598fc3bf1144
Hastimazadeh <hastimazadeh@gmail.com>
2021-12-04 22:45:06 +0100

    changes for details presenter

--- js/vuejs/detailsPresenter.js
+++ js/vuejs/detailsPresenter.js
@@ -2,7 +2,6 @@ function DetailsPresenter(props) {
     return (promiseNoData(props.model.currentMovie, props.model.currentMovieDetails, props.model.currentMovieError) ||
         <DetailsView 
         data = {currentMovieDetails}
-   
         />
     );
 }
