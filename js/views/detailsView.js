a2c8802425bb769c5c8d3b18bf653f2e6da51ebe
amir <amirali.safizadeh@gmail.com>
2021-12-05 16:23:19 +0100

    styling

--- js/views/detailsView.js
+++ js/views/detailsView.js
@@ -5,8 +5,8 @@ function DetailsView(props) {
                 <div class="detinfosection">
                     <div class="detailsbutton">
                         <button
-                            /*onClick={props.dishAdded}
-                            disabled={props.isDishInMenu}*/
+                        /*onClick={props.dishAdded}
+                        disabled={props.isDishInMenu}*/
                         >
                             Add to my Favorites Movies!
                         </button>
@@ -16,22 +16,22 @@ function DetailsView(props) {
                         </button>
                     </div>
                     <div class="detmovieheader">
-                        <img class="detimg" src={props.data.image}/>
+                        <img class="detimg" src={props.data.image} />
                         <h1>{props.data.title}</h1>
                         <h4>year: {props.data.year}, Director: {props.data.directors}</h4>
                         <span class="detminutes">{props.data.runtimeStr}</span>
                         <p class="detgenre">Genre: {props.data.genres}</p>
                         <div class="detmoviedesc">
                             <p class="dettext">
-                                plot: {props.data.plot} 
+                                plot: {props.data.plot}
                             </p>
                         </div>
                     </div>
-                   
-                    
+
+
                 </div>
                 <div class="detblurback">
-                    <img  src={props.data.image}/>
+                    <img class="blurImg" src={props.data.image} />
                 </div>
             </div>
 
