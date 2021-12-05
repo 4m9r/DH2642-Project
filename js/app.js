15519f2f5e22bda6af89abf030531ef3b72eb8fb
Hastimazadeh <hastimazadeh@gmail.com>
2021-12-05 16:51:57 +0100

    style for search view in app.js

--- js/app.js
+++ js/app.js
@@ -3,14 +3,15 @@ function App(props) {
 
     return (
 
-        <div>
-            {/* <QuizPresenter model={props.model} />
-            <Show hash="#list"> <ListPresenter model={props.model} /> </Show> */}
-            <Show hash="#search"> <SearchPresenter model={props.model} /> </Show>
-            <Show hash="#details"> <DetailsPresenter model={props.model} /> </Show>
-            {/* <Show hash="#quiz"> <QuizPresenter model={props.model}/> </Show>
-            <Show hash="#recommend"> <RecommendPresenter model={props.model}/> </Show> */}
-
+        <div  class="flexParent">
+            <div class="mainConetent debug">
+                {/* <QuizPresenter model={props.model} />
+                <Show hash="#list"> <ListPresenter model={props.model} /> </Show> */}
+                <Show hash="#search"> <SearchPresenter model={props.model} /> </Show>
+                <Show hash="#details"> <DetailsPresenter model={props.model} /> </Show>
+                {/* <Show hash="#quiz"> <QuizPresenter model={props.model}/> </Show>
+                <Show hash="#recommend"> <RecommendPresenter model={props.model}/> </Show> */}
+            </div>
         </div>
 
 
