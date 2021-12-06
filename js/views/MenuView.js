7737be26dd40ca3dfa43cb172f59cbaabd451b2c
amir <amirali.safizadeh@gmail.com>
2021-12-06 12:51:57 +0100

    disabling some buttons

--- js/views/MenuView.js
+++ js/views/MenuView.js
@@ -1,17 +1,26 @@
-function MenuView(props){
+function MenuView(props) {
 
-    return(
-      <div class="menudrop">
-        <button class="menubutton">Menu</button>
-        <div class="menucontent">
-          <a href="#list">My list</a>
-          <a href="#search">Search for a movie</a>
-          <a href="#recommend">Recommend me a movie</a>
-          <a href="#quiz">Take a quiz</a>
-        </div>
+  return (
+    <div class="menudrop">
+      <button class="menubutton">Menu</button>
+      <div class="menucontent">
+        <a onClick={() => window.alert("To be implemented")
+        }>My list</a>
+        <a href="#search">Search for a movie</a>
+        <a onClick={() => window.alert("To be implemented")}>Recommend me a movie</a>
+        <a onClick={() => window.alert("To be implemented")}>Take a quiz</a>
       </div>
+    </div>
   )
-  
-    
+
+
 }
-  
+{/* <div class="menudrop">
+<button class="menubutton">Menu</button>
+<div class="menucontent">
+  <a href="#list">My list</a>
+  <a href="#search">Search for a movie</a>
+  <a href="#recommend">Recommend me a movie</a>
+  <a href="#quiz">Take a quiz</a>
+</div>
+</div> */}
