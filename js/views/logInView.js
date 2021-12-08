5022533201392c317c6198c7a19ea5a7a621d274
v-a-p <vaprodan@kth.se>
2021-12-08 16:25:35 +0100

    minor temporary change

--- js/views/logInView.js
+++ js/views/logInView.js
@@ -1,28 +1,18 @@
 function LogInView(props){
 
   return(
-    <html>
-
-    <head>
-
-      <title>
-        Log in!
-      </title>
-
-
-    </head>
-
-    <body>
+    <div>
+    
 
       <h1><input type="text" id="username" placeholder="Username or Email address"/></h1>
       <h1><input type="password" id="password" placeholder="Password"/></h1>
       <button> Log in! </button>
 
-      <span> No account ? </span> <span> &nbsp; </span> <button> Sign up! </button>
+      <span> No account ? </span> <span> &nbsp; </span> <button onClick={SignUpView}> Sign up! </button>
 
-    </body>
+    
 
-    </html>
+    </div>
 )
 
   
