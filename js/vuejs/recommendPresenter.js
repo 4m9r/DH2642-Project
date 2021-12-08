8bf4f11cf3e371db77022208ae6a2ca9383a7719
Hastimazadeh <hastimazadeh@gmail.com>
2021-12-08 14:34:17 +0100

    rec presenter file

---
+++ js/vuejs/recommendPresenter.js
@@ -0,0 +1,11 @@
+function RecommendPresenter(props) {
+    return (promiseNoData(props.model.currentMovie, props.model.currentMovieDetails, props.model.currentMovieError) ||
+        <div>
+            <span> Similar Movies to {props.model.currentMovieDetails.title} </span>
+            <SearchResultsView
+                searchResults={props.model.currentMovieDetails.similars}
+            />
+           
+        </div>
+    );
+}
