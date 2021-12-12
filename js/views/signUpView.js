cff905073027d6d8606a5cd0a6efbf9b7c91e1ac
amir <amirali.safizadeh@gmail.com>
2021-12-12 15:55:07 +0100

    spacing

--- js/views/signUpView.js
+++ js/views/signUpView.js
@@ -1,38 +1,38 @@
 
-function SignUpView(props){
+function SignUpView(props) {
 
-    return(
-      <html>
-  
+  return (
+    <html>
+
       <head>
-  
+
         <title>
           Sign up
         </title>
-  
+
       </head>
-  
+
       <body>
-  
-        <h1><input type="text" id="email" placeholder="Email address"/></h1>
+
+        <h1><input type="text" id="email" placeholder="Email address" /></h1>
         {/* <h1><input type="text" id="emailconfirm" placeholder="Confirm Email address"/></h1> */}
-        <h1><input type="password" id="password" placeholder="Password"/></h1>
+        <h1><input type="password" id="password" placeholder="Password" /></h1>
         {/* <h1><input type="password" id="passwordconfirm" placeholder="Confirm Password"/></h1> */}
-        <h1><input type="text" id="username" placeholder="Create a Username"/></h1>
+        <h1><input type="text" id="username" placeholder="Create a Username" /></h1>
         <div> &nbsp; </div>
         <div> &nbsp; </div>
         <div> &nbsp; </div>
         <div> &nbsp; </div>
         <div> &nbsp; </div>
         <button onclick="signUp()" id="signUp"> Sign Up </button>
-        
+
       </body>
-  
-      </html>
+
+    </html>
   )
-  
-    
-  }
+
+
+}
 
 //   import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
