06383bafdcfb04b60b7e3ab17f40c1066cc2c720
amir <amirali.safizadeh@gmail.com>
2021-12-13 21:48:13 +0100

    section added to display top movies
    becuase we need to display some movies on the first page and our api instead of returing an array returns an object I have to check the incoming data with if statment

--- js/views/searchResultsView.js
+++ js/views/searchResultsView.js
@@ -1,4 +1,28 @@
 function SearchResultsView(props) {
+
+    if (props.searchResults.items) {
+        return (<div >
+            {props.searchResults.items?.slice(0, 16).map(
+                function (item) {
+                    return (
+                        <span class="searchResult" onClick={
+                            e => { props.movieChosen(item.id); window.location.hash = "#details"; }
+                        } >
+                            <img class="searchImage" src={item.image} />
+                            <div>
+                                {item.title}
+                            </div>
+                        </span>
+                    );
+
+                }
+            )
+            }
+
+        </div>
+        );
+    }
+
     return (<div class="results">
         {props.searchResults?.map(
             function (item) {
