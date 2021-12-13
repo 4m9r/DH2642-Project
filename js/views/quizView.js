d85efa53b97e92add5c0a7324a414d87b137b4b0
amir <amirali.safizadeh@gmail.com>
2021-12-13 18:13:24 +0100

    quiz page almost done

--- js/views/quizView.js
+++ js/views/quizView.js
@@ -1,9 +1,20 @@
 function QuizView(props) {
 
-    // console.log(params.details)
-    if (props.model.quizState === true)
+    let scoreCounter = 0;
+    if (props.model.quizState.find((e) => e.movie === props.model.currentMovie))
         return <div>
-            good job you did it your score is 0 <br />
+            good job you did it your score is {(props.model.quizState.find((e) => e.movie === props.model.currentMovie)).score} <br />
+
+            <button onClick={() => {
+                window.location.hash = "#search";
+            }
+            }>
+                Back to the main page
+            </button>
+        </div>
+    if (props.model.currentMovieDetails.similars == null)
+        return <div>
+            This movie does not have a quiz <br />
 
             <button onClick={() => {
                 window.location.hash = "#search";
@@ -13,60 +24,88 @@ function QuizView(props) {
             </button>
         </div>
 
+    let similar1 = props.model.currentMovieDetails.similars[0]
+    let similar2 = props.model.currentMovieDetails.similars[1]
+    let similar3 = props.model.currentMovieDetails.similars[2]
+
+    let theMovie = props.model.currentMovieDetails;
+
+    if (similar1 === undefined || similar2 === undefined || similar3 === undefined)
+        return <div>
+            This movie does not have a quiz <br />
+
+            <button onClick={() => {
+                window.location.hash = "#search";
+            }
+            }>
+                Back to the main page
+            </button>
+        </div>
+
+
     return (
         <div>
             <fieldset>
-                <legend>Question1</legend>
-                <input type="radio" name="action" id="track" value="track" /><label for="track">ooption1</label><br />
-                <input type="radio" name="action" id="event" value="event" /><label for="event">ooption1</label><br />
-                <input type="radio" name="action" id="message" value="message" /><label for="message">ooption1</label><br />
-                <input type="radio" name="action" id="message" value="message" /><label for="message">ooption1</label><br />
+                <legend>Who was the director of the movie "{props.model.currentMovieDetails.title}"?</legend>
+                <input type="radio" name="q1" id="q1o1" /><label for="q1o1">{
+                    (similar1.directors == !theMovie.directors) ? similar1.directors : false || "Ethan Coen"}</label><br />
+                <input type="radio" name="q1" id="q1o2" /><label for="q1o2">{theMovie.directors}</label><br />
+                <input type="radio" name="q1" id="q1o3" /><label for="q1o3">{
+                    (similar2.directors == !theMovie.directors) ? similar2.directors : false ||
+                        "Martin Scorsese"}</label><br />
+                <input type="radio" name="q1" id="q1o4" /><label for="q1o4">{
+                    (similar3.directors == !theMovie.directors) ? similar3.directors : false
+                        || "David Lynch"}</label><br />
             </fieldset>
 
             <fieldset>
-                <legend>Question2</legend>
-                <input type="radio" name="action1" id="track" value="track" /><label for="track">ooption1</label><br />
-                <input type="radio" name="action1" id="event" value="event" /><label for="event">ooption1</label><br />
-                <input type="radio" name="action1" id="message" value="message" /><label for="message">ooption1</label><br />
-                <input type="radio" name="action1" id="message" value="message" /><label for="message">ooption1</label><br />
+                <legend>In what year did the movie "{props.model.currentMovieDetails.title}" get released?</legend>
+                <input type="radio" name="q2" id="q2o1" /><label for="q2o1">{parseInt(theMovie.year) + 5}</label><br />
+                <input type="radio" name="q2" id="q2o2" /><label for="q2o2">{parseInt(theMovie.year) - 10}</label><br />
+                <input type="radio" name="q2" id="q2o3" /><label for="q2o3">{parseInt(theMovie.year)}</label><br />
+                <input type="radio" name="q2" id="q2o4" /><label for="q2o4">{parseInt(theMovie.year) - 15}</label><br />
             </fieldset>
 
             <fieldset>
-                <legend>Question2</legend>
-                <input type="radio" name="action2" id="track" value="track" /><label for="track">ooption1</label><br />
-                <input type="radio" name="action2" id="event" value="event" /><label for="event">ooption1</label><br />
-                <input type="radio" name="action2" id="message" value="message" /><label for="message">ooption1</label><br />
-                <input type="radio" name="action2" id="message" value="message" /><label for="message">ooption1</label><br />
-            </fieldset>
-
-
-            <fieldset>
-                <legend>Question3</legend>
-                <input type="radio" name="action3" id="track" value="track" /><label for="track">ooption1</label><br />
-                <input type="radio" name="action3" id="event" value="event" /><label for="event">ooption1</label><br />
-                <input type="radio" name="action3" id="message" value="message" /><label for="message">ooption1</label><br />
-                <input type="radio" name="action3" id="message" value="message" /><label for="message">ooption1</label><br />
+                <legend>Who are the stars of the movie "{props.model.currentMovieDetails.title}" </legend>
+                <input type="radio" name="q3" id="q3o1" /><label for="q3o1">{similar1.stars}</label><br />
+                <input type="radio" name="q3" id="q3o2" /><label for="q3o2">{similar2.stars}</label><br />
+                <input type="radio" name="q3" id="q3o3" /><label for="q3o3">{similar3.stars}</label><br />
+                <input type="radio" name="q3" id="q3o4" /><label for="q3o4"> {theMovie.stars}</label><br />
             </fieldset>
 
 
             <fieldset>
-                <legend>Question4</legend>
-                <input type="radio" name="action4" id="track" value="track" /><label for="track">ooption1</label><br />
-                <input type="radio" name="action4" id="event" value="event" /><label for="event">ooption1</label><br />
-                <input type="radio" name="action4" id="message" value="message" /><label for="message">ooption1</label><br />
-                <input type="radio" name="action4" id="message" value="message" /><label for="message">ooption1</label><br />
+                <legend>What Imdb score did the movie"{props.model.currentMovieDetails.title}" get? </legend>
+                <input type="radio" name="q4" id="q4o1" /><label for="q4o1">{parseInt(theMovie.imDbRating)}</label><br />
+                <input type="radio" name="q4" id="q4o2" /><label for="q4o2">{parseInt(theMovie.imDbRating) + 0.5}</label><br />
+                <input type="radio" name="q4" id="q4o3" /><label for="q4o3">{parseInt(theMovie.imDbRating) - 1}</label><br />
+                <input type="radio" name="q4" id="q4o4" /><label for="q4o4">{parseInt(theMovie.imDbRating) + 0.3}</label><br />
             </fieldset>
 
+
             <fieldset>
-                <legend>Question5</legend>
-                <input type="radio" name="action5" id="track" value="track" /><label for="track">ooption1</label><br />
-                <input type="radio" name="action5" id="event" value="event" /><label for="event">ooption1</label><br />
-                <input type="radio" name="action5" id="message" value="message" /><label for="message">ooption1</label><br />
-                <input type="radio" name="action5" id="message" value="message" /><label for="message">ooption1</label><br />
+                <legend>Which set of genres describe the movie "{props.model.currentMovieDetails.title}" best? </legend>
+                <input type="radio" name="q5" id="q5o1" /><label for="q5o1">{similar3.genres + " ,Musical"}</label><br />
+                <input type="radio" name="q5" id="q5o2" /><label for="q5o2">{similar1.genres}</label><br />
+                <input type="radio" name="q5" id="q5o3" /><label for="q1o3">{similar2.genres + " ,Sci-Fi"}</label><br />
+                <input type="radio" name="q5" id="q5o4" /><label for="q1o4">{theMovie.genres}</label><br />
             </fieldset>
+
 
             <button onClick={() => {
-                props.model.setStateOfQuize()
+                if (document.getElementById("q1o2").checked)
+                    scoreCounter = scoreCounter + 10;
+                if (document.getElementById("q2o3").checked)
+                    scoreCounter = scoreCounter + 10;
+                if (document.getElementById("q3o4").checked)
+                    scoreCounter = scoreCounter + 10;
+                if (document.getElementById("q4o1").checked)
+                    scoreCounter = scoreCounter + 10;
+                if (document.getElementById("q5o4").checked)
+                    scoreCounter = scoreCounter + 10;
+                // props.model.setUserTotalScore(scoreCounter);
+                props.model.setQuizState({ movie: props.model.currentMovie, score: scoreCounter })
             }
             }>
                 Finish
@@ -74,39 +113,3 @@ function QuizView(props) {
         </div >
     )
 }
-
-
-// function btnName(x) {
-//     console.log(x)
-//     if (x === 2)
-//         return "Finish"
-//     else
-//         return "Next"
-// }
-
-
-{/* <div>
-                    <span>Quiz about {params.details.title} <br /> </span>
-                    <span> {params.quizData[params.model.numberOfQuestion].Question} </span>
-                    <div>
-                        <input type="radio" id="o1" name="fav_language" value="HTML" />
-                        <label for="o1">{eval(params.quizData[params.model.numberOfQuestion].option1)}</label>
-                        <button >{eval(params.quizData[params.model.numberOfQuestion].option1)}</button>
-                        <button>{params.quizData[params.model.numberOfQuestion].option2}</button>
-                        <button>{params.quizData[params.model.numberOfQuestion].option3}</button>
-                        <button>{params.quizData[params.model.numberOfQuestion].option4}</button>
-
-                    </div>
-                    <div>
-                        <button disabled={params.model.numberOfQuestion == 0} onClick={() =>
-                            params.model.setQuestionNumber(params.model.numberOfQuestion - 1)}>
-                            Privous
-                        </button>
-                        <button onClick={() =>
-                            params.model.setQuestionNumber(params.model.numberOfQuestion + 1)}>
-                            {
-                                btnName(params.model.numberOfQuestion)
-                            }
-                        </button>
-                    </div>
-                </div> */}
