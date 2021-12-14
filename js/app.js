f8d4a2c3f532b0fb6b8e7b550336af33fbd62749
Hastimazadeh <hastimazadeh@gmail.com>
2021-12-14 03:16:53 +0100

    testing for routing

--- js/app.js
+++ js/app.js
@@ -21,9 +21,28 @@ function App(props) {
 
             </div>
 
+            <div class="mainConetent">
+                <div class="navbar">
+                    <span class="logo">FilmBuff</span>
+                    <span > <MenuView /></span>
+                </div>
+
+                {/* <Show hash="#list"> <ListPresenter model={props.model} /> </Show> */}
+                <Show hash="#search"> <SearchPresenter model={props.model} /> </Show>
+                <Show hash="#details"> <DetailsPresenter model={props.model} /> </Show>
+                <Show hash="#user"> <ListPresenter model={props.model}/> </Show>
+                <Show hash="#rec"> <RecommendPresenter model={props.model} /> </Show>
+                <Show hash="#signup"> <SignUpPresenter model={props.model}/> </Show>
+                <Show hash="#login"> <LogInPresenter model={props.model} /> </Show>
+                <Show hash="#quiz"> <QuizPresenter model={props.model} /> </Show>
+
+            </div>
+
             <div class="userlist debug"><UserListPresenter model={props.model} /></div>
 
         </div>
+
+        
 
 
     );
