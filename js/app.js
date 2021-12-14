87c8fd15d019e60bc152bb6d0c0251dd9a704510
amir <amirali.safizadeh@gmail.com>
2021-12-14 13:59:08 +0100

    spacing

--- js/app.js
+++ js/app.js
@@ -14,9 +14,9 @@ function App(props) {
                 {/* <Show hash="#list"> <ListPresenter model={props.model} /> </Show> */}
                 <Show hash="#search"> <SearchPresenter model={props.model} /> </Show>
                 <Show hash="#details"> <DetailsPresenter model={props.model} /> </Show>
-                <Show hash="#user"> <ListPresenter model={props.model}/> </Show>
+                <Show hash="#user"> <ListPresenter model={props.model} /> </Show>
                 <Show hash="#rec"> <RecommendPresenter model={props.model} /> </Show>
-                <Show hash="#signup"> <SignUpPresenter model={props.model}/> </Show>
+                <Show hash="#signup"> <SignUpPresenter model={props.model} /> </Show>
                 <Show hash="#login"> <LogInPresenter model={props.model} /> </Show>
                 <Show hash="#quiz"> <QuizPresenter model={props.model} /> </Show>
 
@@ -36,10 +36,8 @@ let user = auth.currentUser;
 
 // Not working 
 function defaultRoute() {
-    if (user) 
-        {if (!(["#search", "#details", "#user", "#signup", "#login", "#rec", "#quiz"].find((knownRoute) => window.location.hash == knownRoute))) window.location.hash = "#search";}
-    else
-        {if (!(["#search", "#details", "#user", "#signup", "#login", "#rec", "#quiz"].find((knownRoute) => window.location.hash === knownRoute))) window.location.hash = "#login";}
+    if (user) { if (!(["#search", "#details", "#user", "#signup", "#login", "#rec", "#quiz"].find((knownRoute) => window.location.hash == knownRoute))) window.location.hash = "#search"; }
+    else { if (!(["#search", "#details", "#user", "#signup", "#login", "#rec", "#quiz"].find((knownRoute) => window.location.hash === knownRoute))) window.location.hash = "#login"; }
 }
 
 
