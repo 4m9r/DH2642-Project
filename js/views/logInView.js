ca7a98ac67dfc63bb760847b1a9e996142097a27
v-a-p <vaprodan@kth.se>
2021-12-14 02:09:57 +0100

    styling for authentication

--- js/views/logInView.js
+++ js/views/logInView.js
@@ -1,15 +1,24 @@
 function LogInView(props) {
 
   return (
+    <div class="authbox2">
+
     <div>
-
-
-      <h1><input type="text" id="emaillog" placeholder="Email address" /></h1>
-      <h1><input type="password" id="passwordlog" placeholder="Password" /></h1>
-      <button onclick={() => logIn()}> Log in </button>
-
-      <span> No account ? </span> <span> &nbsp; </span> <button onClick={() => window.location.hash = "#signup"}> Sign up! </button>
-
+        <h2 class="titletext2">
+          LOG IN
+        </h2>
+    </div>
+      <h1><input class="inputbox" type="text" id="emaillog" placeholder="Email address" /></h1>
+      <h1><input class="inputbox" type="password" id="passwordlog" placeholder="Password" /></h1>
+      <div> &nbsp; </div>
+      <div class="authbuttonstyle">
+      <button class="authbutton" onclick={() => logIn()}> Log in </button>
+      
+      <div>
+      <div> &nbsp; </div>
+      <span class="funtext"> No account ? </span> <div> &nbsp; </div> <button class="authbutton" onClick={() => window.location.hash = "#signup"}> Sign up </button>
+      </div>
+      </div>
 
 
     </div>
--- js/views/signUpView.js
+++ js/views/signUpView.js
@@ -3,22 +3,31 @@ function SignUpView(props) {
 
   return (
 
+    
+    
+    <div class="authbox2">
+
     <div>
-
-      <h1><input type="text" id="email" placeholder="Email address" /></h1>
-      {/* <h1><input type="text" id="emailconfirm" placeholder="Confirm Email address"/></h1> */}
-      <h1><input type="password" id="password" placeholder="Password" /></h1>
-      {/* <h1><input type="password" id="passwordconfirm" placeholder="Confirm Password"/></h1> */}
-      <h1><input type="text" id="username" placeholder="Create a Username" /></h1>
-      <div> &nbsp; </div>
-      <div> &nbsp; </div>
-      <div> &nbsp; </div>
-      <div> &nbsp; </div>
-      <div> &nbsp; </div>
-      <button onclick={() => signUp(props.model)} > Sign Up </button>
-
+        <h2 class="titletext">
+          CREATE YOUR ACCOUNT
+        </h2>
     </div>
+      
 
+
+      <h1><input class="inputbox" type="text" id="email" placeholder="Email address" /></h1>
+      {/* <h1><input type="text" id="emailconfirm" placeholder="Confirm Email address"/></h1> */}
+      <h1><input class="inputbox" type="password" id="password" placeholder="Password" /></h1>
+      {/* <h1><input type="password" id="passwordconfirm" placeholder="Confirm Password"/></h1> */}
+      <h1><input class="inputbox" type="text" id="username" placeholder="Create a Username" /></h1>
+
+      <div>&nbsp;</div>
+
+      <div class="authbuttonstyle">
+      <button class="authbutton" onclick={() => signUp(props.model)} > Sign Up </button>
+      </div>
+    </div>
+    
 
   )
 
