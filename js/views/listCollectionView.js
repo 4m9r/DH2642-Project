ba1b398d59e2e743b75bf596c8a93db39ee49816
danikagiron <danikag@kth.se>
2021-12-15 19:25:59 +0100

    More user/list css

--- js/views/listCollectionView.js
+++ js/views/listCollectionView.js
@@ -3,46 +3,42 @@ function ListCollectionView(props) {
 
     return (
 
+        <div>
 
-        <div class="card-container">
-            <span> <img src={props.profilePic} class="profile-pic-circle" /> </span>
+            <div class="card-container">
+                <span> <img src={props.profilePic} class="profile-pic-circle" /> </span>
 
-            <p>
-                <b><h2 stlye="padding: 5px;">{props.username}</h2></b>
+                <p>
+                    <b><h2 stlye="padding: 5px;">{props.username}</h2></b>
+                    <br />
+                    <div class="profile-description">Welcome to my profile, check out the movies I like!</div>
+                </p>
                 <br />
-                <div class="profile-description">Welcome to my profile, check out the movies I like!</div>
-            </p>
-            <br />
-            <div>
-                <button class="footer-button">
-                    <img src="https://cdn-icons-png.flaticon.com/512/61/61342.png" height={25} style="padding-top: 5px;" />
-                    <p>Films</p>
-                </button>
-                <button class="footer-button">
-                    <img src="https://www.pngall.com/wp-content/uploads/4/Heart-Symbol-Transparent.png" height={25} style="padding-top: 5px;" />
-                    <p>Lists</p>
-                </button>
-                <button class="footer-button">
-                    <img src="https://cdn-icons-png.flaticon.com/512/32/32441.png" height={25} style="padding-top: 5px;" />
-                    <p>Friends</p>
-                </button>
+                <div>
+                    <button class="footer-button">
+                        <img src="https://cdn-icons-png.flaticon.com/512/61/61342.png" height={25} style="padding-top: 5px;" />
+                        <p>Films</p>
+                    </button>
+                    <button class="footer-button">
+                        <img src="https://www.pngall.com/wp-content/uploads/4/Heart-Symbol-Transparent.png" height={25} style="padding-top: 5px;" />
+                        <p>Lists</p>
+                    </button>
+                    <button class="footer-button">
+                        <img src="https://cdn-icons-png.flaticon.com/512/32/32441.png" height={25} style="padding-top: 5px;" />
+                        <p>Friends</p>
+                    </button>
+                </div>
             </div>
 
 
-            {/* // <div >
-        //     {props.lists.map(x =>
-        //         <div class="listbox">
-        //             <span>
-        //                 <p style="margin-bottom: 10px;"><b>{x.listname}</b></p>
-        //                 {/* {x.map(movie => 
-        //                     <p> {movie} </p>
-        //                     )} */}
-            {/* //             </span> */}
+            <div class="card-container" stlye="display: inline-block;">
+                <p> All the films watched: </p>
+                
+            </div>
+
 
         </div>
-        //     )}
 
-        // </div> */}
 
 
     )
