dffd9ea59e1dfdff6b28af76f23c10da19d0bf0f
amir <amirali.safizadeh@gmail.com>
2021-12-16 06:17:05 +0100

    remove the commented out sections and added some object properties

--- js/firebaseModel.js
+++ js/firebaseModel.js
@@ -1,52 +1,46 @@
 function persistModel(model) {
-    let user = firebase.auth().currentUser;
-    //let loadingFromFirebase = false;
+    const auth = firebase.auth();
+    let loadingFromFirebase = false;
     model.addObserver(function () {
-        // if (loadingFromFirebase)
-        //     return;
-        firebase.database().ref("user").child(user.uid).set({  // object literal
-            currentMovie: model.currentMovie,
-            username: model.username,
-            quizState: model.quizState,
-            totalScore: model.totalScore
-        });
-        firebase.database().ref("learboard").child(model.userNumber).set({
-            username: model.username,
-            totalScore: model.totalScore
-        })
+        if (loadingFromFirebase)
+            return;
+        console.log(auth.currentUser.uid)
+        console.log(model.userID)
+        if (auth.currentUser) {
+            firebase.database().ref("user").child(auth.currentUser.uid).set({  // object literal
+                currentMovie: model.currentMovie,
+                username: model.username,
+                quizState: model.quizState,
+                totalScore: model.totalScore,
+                userID: model.userID,
+                userNumber: model.userNumber
+            });
+            firebase.database().ref("learboard").child(model.userNumber).set({
+                username: model.username,
+                totalScore: model.totalScore
+            })
+        }
     });
 
+    if (auth.currentUser)
+        firebase.database().ref("user").child(auth.currentUser.uid).child("userModel").on("value", function (data) {
+            loadingFromFirebase = true;
+            try {
+                if (data.val()) {
+                    model.setCurrentMovie(data.val().currentMovie || null);
+                    model.setUser(data.val().userID || null,
+                        data.val().username || null)
+                    model.setUserNumber(data.val().userNumber || 0)
+                    model.setQuizState(data.val().quizState || [])
+                    model.setUserTotalScore(data.val().totalScore || 0)
+                }
+            }
+            catch (e) {
+                console.log(e);
+            }
+            loadingFromFirebase = false;
+        })
 
-
-
-    // firebase.database().ref("user").child(user.uid).child("userModel").on("value", function (data) {
-    //     try {
-    //         if (data.val()) {
-    //             model.setCurrentMovie(data.val().currentMovie || null);
-    //             model.setUsername(data.val().username || null)
-    //         }
-    //     }
-    //     catch (e) {
-    //         console.log(e);
-    //     }
-    // })
-
-    // firebase.database().ref("user/leaderBoardScore").child(model.username).set({  // object literal
-    //     totalScore: model.totalScore,
-    // });
-
-
-
-    // firebase.database().ref("user").child("ENbGmOTG0MY3RCskym96V5NYZQH3").child("username").on("value", function (data) {
-    //     try {
-    //         if (data.val()) {
-    //                model.setUsername(data.val() || null);
-    //         }
-    //     }
-    //     catch (e) {
-    //         console.log(e);
-    //     }
-    // })
 
     firebase.database().ref("user").child("ENbGmOTG0MY3RCskym96V5NYZQH3/usersList").once("value", function (data) {
         try {
@@ -63,17 +57,6 @@ function persistModel(model) {
     firebase.database().ref('/user/ENbGmOTG0MY3RCskym96V5NYZQH3/profilepic').once('value').then(function (snapshot) {
         model.setProfilePic(snapshot.val());
     })
-
-
-    // firebase.database().ref("question").on("value", function (data) {
-    //     try {
-    //         if (data.val())
-    //             model.setQuizList(data.val())
-    //     }
-    //     catch (e) {
-    //         console.log(e);
-    //     }
-    // })
 
 
     firebase.database().ref('/friends/').once('value').then(function (snapshot) {
