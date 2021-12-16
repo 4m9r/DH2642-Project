9127b1364efceb1242e248479071f57af460a4ea
amir <amirali.safizadeh@gmail.com>
2021-12-16 20:25:24 +0100

    leaderboardpresenter compeleted

--- js/vuejs/leaderBoardPresenter.js
+++ js/vuejs/leaderBoardPresenter.js
@@ -1,32 +1,14 @@
-let leaderboard = [];
 
-function LeaderboardPresenter() {
-    console.log(leaderboard);
-    let x;
-    x=LeaderboardData();
-    console.log(x);
-    return (
-        <div>
-            <LeaderboardView
-            
-                userData={x}
 
-            
-            />
-        </div>
-    );
-}
+function LeaderboardPresenter(props) {
+    // console.log(props.model.leaderboardData)
+    if (props.model.leaderboardData)
+        return (
+            <div>
+                <LeaderboardView
+                    userData={props.model.leaderboardData}
 
-function LeaderboardData() {
-    
-    firebase.database().ref("learboard").once("value", function (data) {
-        console.log(data.val());
-        try{
-            if(data.val())
-                leaderboard = data.val() || [];
-                console.log(leaderboard);
-                return leaderboard;
-        }
-        catch(e){console.log(e)}
-    })
+                />
+            </div>
+        );
 }
