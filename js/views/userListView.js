85eb6d17b2c34ad98db5a06420e61c5c41f0f826
danikagiron <danikag@kth.se>
2021-12-17 10:43:56 +0100

    Update userListView

--- js/views/userListView.js
+++ js/views/userListView.js
@@ -6,7 +6,7 @@ function UserListView(props) {
             <div style="overflow-y: scroll;">
                 <p style="margin-bottom: 15px;color: white;"><b>Find out what others have been watching!</b></p>
 
-                {props.users.map(x =>
+                {((props.users).filter(f=> f.userNumber!=0)).map(x =>
 
                     <span class="userlist-display" onClick={e=> (props.setCurrentFriendView(x), window.location.hash = "#user")}>
                         <img class="profile-pic" src={x.profilePic} />
@@ -15,6 +15,7 @@ function UserListView(props) {
                         <p class="userlist-score"><img src="https://www.pinclipart.com/picdir/big/355-3556122_4-succeed-vector-trophy-icon-png-clipart.png" height={10} style="color:white;"/>{x.totalScore}</p>
                         </span>
                     </span>
+                   
 
                 )}
 
