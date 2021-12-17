75a8fda01974313ff6263683af2874c8ac87cd10
danikagiron <danikag@kth.se>
2021-12-17 16:45:18 +0100

    Updating from last push

--- js/vuejs/navPresenter.js
+++ js/vuejs/navPresenter.js
@@ -1,9 +1,9 @@
 function NavPresenter(props){
     return(
         <div class="navbar">
-                        <span class="siteName">
-                             <img class="icon" src="https://www.freeiconspng.com/uploads/film-media-movie-scene-start-tv-video-icon--1.png" onClick={()=> window.location.hash = "#search"}/> 
-                             <span class="logo" onClick={()=> window.location.hash = "#search"}>FilmBuff</span>
+                        <span class="siteName"  onClick={()=> window.location.hash = "#search"}>
+                             <img class="icon" src="https://www.freeiconspng.com/uploads/film-media-movie-scene-start-tv-video-icon--1.png"/> 
+                             <span class="logo">FilmBuff</span>
                         </span>
                         
                         <span > <MenuView /></span>
