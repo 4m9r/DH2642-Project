8c983537d4ed0691dee59a1eb14ad5099bb59b0d
danikagiron <danikag@kth.se>
2021-12-17 16:44:33 +0100

    Clicking on the logo will take you back to #search
    we like the details

--- js/vuejs/navPresenter.js
+++ js/vuejs/navPresenter.js
@@ -2,8 +2,8 @@ function NavPresenter(props){
     return(
         <div class="navbar">
                         <span class="siteName">
-                             <img class="icon" src="https://www.freeiconspng.com/uploads/film-media-movie-scene-start-tv-video-icon--1.png"/> 
-                             <span class="logo">FilmBuff</span>
+                             <img class="icon" src="https://www.freeiconspng.com/uploads/film-media-movie-scene-start-tv-video-icon--1.png" onClick={()=> window.location.hash = "#search"}/> 
+                             <span class="logo" onClick={()=> window.location.hash = "#search"}>FilmBuff</span>
                         </span>
                         
                         <span > <MenuView /></span>
