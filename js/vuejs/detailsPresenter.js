657c4917262fb7e403427d4dd7f206b2f2553200
danikagiron <danikag@kth.se>
2021-12-17 15:40:38 +0100

    Fixing the "add to favourites" button to be disabled properly

--- js/vuejs/detailsPresenter.js
+++ js/vuejs/detailsPresenter.js
@@ -2,7 +2,7 @@ function DetailsPresenter(props) {
     return (promiseNoData(props.model.currentMovie, props.model.currentMovieDetails, props.model.currentMovieError) ||
         <DetailsView
             addToList={e => props.model.addToFavourites(e)}
-            isMovieInFavourites={props.model.favourites.find(d => d == props.model.currentMovieDetails)}
+            isMovieInFavourites={props.model.favourites.find(d => d.id== props.model.currentMovieDetails.id)}
             currentMovie={props.model.currentMovie}
             data={props.model.currentMovieDetails}
 
