bc2bdc45140830b7735d0a7667ab05c67d9221e1
Hastimazadeh <hastimazadeh@gmail.com>
2021-12-18 02:04:15 +0100

    remoeMovie added

--- js/vuejs/detailsPresenter.js
+++ js/vuejs/detailsPresenter.js
@@ -2,6 +2,8 @@ function DetailsPresenter(props) {
     return (promiseNoData(props.model.currentMovie, props.model.currentMovieDetails, props.model.currentMovieError) ||
         <DetailsView
             addToList={e => props.model.addToFavourites(e)}
+            //NEW
+            removeMovie={e => props.model.removeFromFavList(e)}
             isMovieInFavourites={props.model.favourites.find(d => d.id== props.model.currentMovieDetails.id)}
             currentMovie={props.model.currentMovie}
             data={props.model.currentMovieDetails}
