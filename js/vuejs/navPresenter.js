ca85fef671686df61df0d3fc1a0313efe696b9da
amir <amirali.safizadeh@gmail.com>
2021-12-18 23:36:52 +0100

    clicking on logo directs us to main page

--- js/vuejs/navPresenter.js
+++ js/vuejs/navPresenter.js
@@ -1,11 +1,11 @@
-function NavPresenter(props){
-    return(
+function NavPresenter(props) {
+    return (
         <div class="navbar">
-            <span class="siteName">
-                <img class="icon" src="https://www.freeiconspng.com/uploads/film-media-movie-scene-start-tv-video-icon--1.png"/> 
+            <span class="siteName" onClick={() => window.location.hash = "#search"}>
+                <img class="icon" src="https://www.freeiconspng.com/uploads/film-media-movie-scene-start-tv-video-icon--1.png" />
                 <span class="logo">FilmBuff</span>
             </span>
-                        
+
             <span > <MenuView /></span>
         </div>
     );
