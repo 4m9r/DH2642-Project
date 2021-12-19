0967e61e269419c5cf8604217c459dcf4b3b1c73
Hastimazadeh <hastimazadeh@gmail.com>
2021-12-19 16:05:34 +0100

    trying to fix refresh thingy

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 function defaultRoute() {
     const user = firebase.auth().currentUser
-    if (user) { if (!(["#search", "#details", "#user", "#rec", "#quiz", "#leaderboard", "#profile"].find((knownRoute) => window.location.hash == knownRoute))) window.location.hash = "#search"; }
-    else { if (!(["#signup", "#login"].find((knownRoute) => window.location.hash === knownRoute))) window.location.hash = "#login"; }
+    if (user) { 
+        console.log(window.location.hash);
+
+        /*if (!(["#search", "#details", "#user", "#rec", "#quiz", "#leaderboard", "#profile"].find((knownRoute) => window.location.hash == knownRoute))) window.location.hash = "#search"; */
+        if (window.location.hash != "#details" && window.location.hash != "#user"
+        && window.location.hash != "#search" && window.location.hash != "#rec" 
+        && window.location.hash != "#quiz" && window.location.hash != "#leaderboard" 
+        && window.location.hash != "#profile") 
+        
+            { window.location.hash = "#search"; }
+    }
+    else { 
+        /*if (!(["#signup", "#login"].find((knownRoute) => window.location.hash === knownRoute))) window.location.hash = "#login"; */
+        if (window.location.hash != "#signup" && window.location.hash != "#login") 
+            { window.location.hash = "#login"; }
+            console.log(window.location.hash);
+
+    }
 }
 
 const App = {
@@ -32,7 +48,6 @@ const App = {
                     <Show hash="#login"> <LogInPresenter model={this.model} /> </Show>
                     <Show hash="#leaderboard"> <LeaderboardPresenter model={this.model} /> </Show>
                     <Show hash="#quiz"> <QuizPresenter model={this.model} /> </Show>
-
                 </div>
                 {this.user && <div class="userlist debug"><UserListPresenter model={this.model} /></div>}
             </div>
